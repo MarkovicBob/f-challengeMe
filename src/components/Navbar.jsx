63ba9a070f69aab20a2b34c9ed16ed1c5365b47a
ramil_novruzov <ramil.novruzov@gmx.de>
2025-05-06 12:25:25 +0200

    added dynamic titles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,31 @@
 import logo from "../assets/logo.png";
+import { useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation();
+
+  const pageTitles = {
+    "/start/home": "Challenges",
+    "/start/createchallenge": "Create Challenge",
+    "/start/challengeprogress": "Challenge Progress",
+    "/start/map": "Map",
+    "/start/takeaphoto": "Take a Photo",
+    "/start/dashboard": "Dashboard",
+    "/onboarding": "Onboarding",
+    "/": "Welcome",
+  };
+
+  let pageTitle = "Challenge ME";
+
+  if (
+    location.pathname.startsWith("/start/home/") &&
+    location.pathname.split("/").length === 4
+  ) {
+    pageTitle = "Challenge Detail";
+  } else if (pageTitles[location.pathname]) {
+    pageTitle = pageTitles[location.pathname];
+  }
+
   return (
     <>
       <nav className="flex items-center w-full bg-gray-700 fixed top-0 z-50">
@@ -8,7 +33,7 @@ function Navbar() {
           <img className="w-15" src={logo} alt="logo" />
         </div>
         <div className="absolute left-1/2 transform -translate-x-1/2">
-          Challenge ME
+          {pageTitle}
         </div>
       </nav>
     </>
